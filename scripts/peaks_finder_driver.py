ffd2ce9d94e76e07cb9376aba0ccec4d2d994c97
kevinliang43 <kevinliang43@gmail.com>
2020-02-28 20:50:28 -0500

    Added functionality for taking entire directory as input for the finding peaks utility

--- scripts/peaks_finder_driver.py
+++ scripts/peaks_finder_driver.py
@@ -1,7 +1,12 @@
 import sys
+import os
 import pandas as pd
 from peaks_utils import clean_table, find_peaks, save_peaks_as_excel
-from utils import parse_inputs, create_default_output_path
+from utils import parse_inputs, create_default_output_path, list_directory_files
+
+SHEET_NAME = 'Peaks Results'
+INPUT_COLUMNS = ['Centroid Mass', 'Relative Intensity']  # Columns of Interest in input files
+OUTPUT_COLUMNS = ['Centroid Mass 1', 'Relative Intensity 1', 'Centroid Mass 2', 'Relative Intensity Mass 2']
 
 
 def main():
@@ -18,19 +23,52 @@ def main():
     if not output_fp:
         output_fp = create_default_output_path()
 
-    # Read File
-    table_df = pd.read_excel(input_fp)
+    # Mapping of file_name to file_path
+    files_mapping = {}
 
-    # Clean and parse data
-    clean_table_df = clean_table(table_df)
-    columns = ['Centroid Mass', 'Relative Intensity']
-    mass_intensity_df = clean_table_df[columns]
+    # If input path is a file
+    if os.path.isfile(input_fp):
+        files_mapping = {os.path.split(input_fp)[1]: input_fp}
 
-    # Discover peaks
-    result = find_peaks(mass_intensity_df, intensity_tolerance, mass_shift, mass_tolerance)
+    # If input path is a directory
+    elif os.path.isdir(input_fp):
+        files_mapping = list_directory_files(input_fp)
 
-    # Write to output
-    save_peaks_as_excel(result, output_fp)
+    # Initialize Excel Writer
+    output_writer = pd.ExcelWriter(output_fp, engine='xlsxwriter')
+    workbook = output_writer.book
+    worksheet = workbook.add_worksheet(SHEET_NAME)
+    output_writer.sheets[SHEET_NAME] = worksheet
+
+    # Initialize Pointer to the current row that is up next to write in
+    current_row = 0
+
+    # Iterate through all files and write to single excel file
+    for file_name, file_path in files_mapping.items():
+
+        # Read File
+        table_df = pd.read_excel(file_path)
+
+        # Clean and parse data
+        clean_table_df = clean_table(table_df)
+        mass_intensity_df = clean_table_df[INPUT_COLUMNS]
+
+        # Discover peaks
+        result = find_peaks(mass_intensity_df, intensity_tolerance, mass_shift, mass_tolerance)
+
+        # Write File Name as the header for the current data section.
+        worksheet.write_string(current_row, 0, file_name)
+        current_row += 1  # Increment row pointer
+
+        # Write Peaks Data
+        df = pd.DataFrame(result, columns=OUTPUT_COLUMNS)
+        df.to_excel(output_writer, sheet_name=SHEET_NAME, startrow=current_row, startcol=0)
+
+        # Increment current_row pointer
+        current_row += df.shape[0] + 2  # Number of rows of data written plus 2 rows for buffer
+
+    # Close Output writer
+    output_writer.close()
 
 
 if __name__ == "__main__":
